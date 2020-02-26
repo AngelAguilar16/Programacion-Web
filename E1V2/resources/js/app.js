cde4e96fdd2ca46574d8a9463f0253bf78338f0a
Angel Aguilar <mgordillo0@ucol.mx>
2020-02-25 20:16:45 -0600

    Change folder structure

--- E1V2/resources/js/app.js
+++
@@ -1,7 +0,0 @@
-function verFoto(e){
-    const mf = document.querySelector("#modal-foto");
-    mf.innerHTML = `
-        <img src="${ e.getAttribute("src") }" alt="Foto" onclick="this.parentElement.style = display='none'">
-    `;
-    mf.style.display = 'block';
-}
