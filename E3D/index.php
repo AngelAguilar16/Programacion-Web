b74cc70a3422782f7a3e62649d4a6e47f6c88f92
Angel Aguilar <mgordillo0@ucol.mx>
2020-05-05 18:18:01 -0500

    Create E3D

---
+++ E3D/index.php
@@ -0,0 +1,4 @@
+<?php
+
+
+header("location: resources/views/home.php");
---
+++ E3D/resources/views/layouts/main.php
@@ -0,0 +1,32 @@
+<?php
+
+function head(){
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/resources/css/bootstrapp.min.css">
+    <link rel="stylesheet" href="/resources/css/style.css">
+    <title>BLOGX</title>
+</head>
+<body>
+    <nav class="navbar navbar-expanded-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">BLOGX</a>
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Intercambiar navegacion">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse-navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active"></li>
+            </ul>
+        </div>    
+    </nav>
+    
+</body>
+</html>
+
+<?php
+}
